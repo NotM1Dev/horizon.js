1d9ad534de8e682614b9edb12654c66a0b006448
m1-dev <140819896+m1-dev@users.noreply.github.com>
2024-01-07 21:34:02 +0200

    refactor: use proper variable name

--- packages/horizon.js/src/functions/loadEvents.ts
+++ packages/horizon.js/src/functions/loadEvents.ts
@@ -8,20 +8,22 @@ import path from 'node:path';
 
 interface LoadEventsParams {
   client: Client;
-  dir: string;
+  directory: string;
 }
 
 export async function loadEvents(params: LoadEventsParams): Promise<void> {
-  const { client, dir } = params;
-  const folders = await fs.readdir(dir);
+  const { client, directory } = params;
+  const folders = await fs.readdir(directory);
 
   for (const folder of folders) {
-    const files = (await fs.readdir(path.join(dir, folder))).filter((file) =>
+    const files = (await fs.readdir(path.join(directory, folder))).filter((file) =>
       allowedExtensions.some((ext) => file.endsWith(ext))
     );
 
     for (const file of files) {
-      let event: Event & { default?: Event } = await import(path.join(dir, folder, file));
+      let event: Event & { default?: Event } = await import(
+        path.join(directory, folder, file)
+      );
 
       if (event.default) {
         event = event.default;
@@ -30,7 +32,7 @@ export async function loadEvents(params: LoadEventsParams): Promise<void> {
       if (!event.name || !event.execute) {
         process.emitWarning(
           `Event module located at ${path.join(
-            dir,
+            directory,
             folder,
             file
           )} does not export a required name or execute property.`
